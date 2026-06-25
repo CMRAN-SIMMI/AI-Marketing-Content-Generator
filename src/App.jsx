542d69b2d08507589beba34c85667ea289f33645
Simran <cmran195@gmail.com>
2026-06-25 13:38:07 +0530

    Improved dark mode support and UI consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,3 @@
-// import { Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Generate from "./pages/Generate";
-// import Assistant from "./pages/Assistant";
-// import History from "./pages/History";
-// import Login from "./pages/Login";
-// import ComponentsDemo from "./pages/ComponentsDemo";
-
-// function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route path="/generate" element={<Generate />} />
-//       <Route path="/assistant" element={<Assistant />} />
-//       <Route path="/history" element={<History />} />
-//       <Route path="/login" element={<Login />} />
-//       <Route path="/components-demo" element={<ComponentsDemo />} />
-//     </Routes>
-    
-//   );
-// }
-
-// export default App;
-
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
